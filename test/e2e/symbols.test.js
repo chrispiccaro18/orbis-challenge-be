83d8539bde13261d6d2079a199846166072e013b
chrispiccaro <chrispiccaro18@gmail.com>
2020-04-23 20:41:18 -0700

    symbols route with test

--- test/e2e/symbols.test.js
+++ test/e2e/symbols.test.js
@@ -1,14 +1,26 @@
-// require('../connect-db');
 const request = require('supertest');
 const app = require('../../lib/app');
+// const { stockwitsSingleStockResponse } = require('../mock-data');
+
+jest.mock('../../lib/services/stocktwits-api.js', () => () => {
+  return {
+    body: {
+      messages: [
+        { test: 'test' },
+      ]
+    }
+  };
+});
 
 describe('symbols route', () => {
   it('returns tweets of a given stock', async() => {
     const response = await request(app)
-      .get('/api/v1/symbols/AAPL');
+      .get('/api/v1/symbols/aapl');
 
     expect(response.body).toEqual({
-      message: 'hi'
+      messages: [
+        { test: 'test' },
+      ]
     });
   });
 });
